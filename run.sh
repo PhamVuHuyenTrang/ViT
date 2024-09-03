ed3ab46102d301ab3a74a5c8a8a7356bbbc35f11
Huyền Trang <98087513+PhamVuHuyenTrang@users.noreply.github.com>
2024-09-03 11:16:56 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,14 @@
-###### FOR TRAINING
-CUDA_VISIBLE_DEVICES='3, 4' python -m torch.distributed.launch --master_port 10020 --nproc_per_node=2 --use_env main_train.py \
---model deit_tiny_patch16_224 --batch-size 256 --data-path /vinai/trangpvh1/imagenet/ --output_dir /home/ubuntu/iclr_prj/checkpoints --u_momentum 0.5 --s_momentum 0.01 --method momentum  \
-
-###### FOR ROBUSTNESS EVAL
-# CUDA_VISIBLE_DEVICES='0,1,2,3' python -m torch.distributed.launch --master_port 1 --nproc_per_node=4 --use_env eval_OOD.py \
-# --model deit_tiny_patch16_224 --data-path /path/to/data/imagenet/ --output_dir /path/to/checkpoints/ \
-# --robust --num_iter 4 --lambd 4 --layer 0 --resume /path/to/model/checkpoint/
+CUDA_VISIBLE_DEVICES='4,5,6,7' python -m torch.distributed.launch --master_port 10020 --nproc_per_node=4 --use_env main_train.py \
+--model deit_tiny_patch16_224 --batch-size 256 --data-path /root/data/imagenet/ --output_dir /root/checkpoints_trang/rpc_tiny/ --hyperparam_1 1.0 --hyperparam_2 1.0 --method vit  &
+CUDA_VISIBLE_DEVICES='4,5,6,7' python -m torch.distributed.launch --master_port 10020 --nproc_per_node=4 --use_env main_train.py \
+--model deit_tiny_patch16_224 --batch-size 256 --data-path /root/data/imagenet/ --output_dir /root/checkpoints_trang/rpc_tiny/ --hyperparam_1 0.1 --hyperparam_2 1.0 --method moving_average &
+CUDA_VISIBLE_DEVICES='4,5,6,7' python -m torch.distributed.launch --master_port 10020 --nproc_per_node=4 --use_env main_train.py \
+--model deit_tiny_patch16_224 --batch-size 256 --data-path /root/data/imagenet/ --output_dir /root/checkpoints_trang/rpc_tiny/ --hyperparam_1 0.3 --hyperparam_2 1.0 --method moving_average &
+CUDA_VISIBLE_DEVICES='4,5,6,7' python -m torch.distributed.launch --master_port 10020 --nproc_per_node=4 --use_env main_train.py \
+--model deit_tiny_patch16_224 --batch-size 256 --data-path /root/data/imagenet/ --output_dir /root/checkpoints_trang/rpc_tiny/ --hyperparam_1 0.6 --hyperparam_2 1.0 --method moving_average &
+CUDA_VISIBLE_DEVICES='4,5,6,7' python -m torch.distributed.launch --master_port 10020 --nproc_per_node=4 --use_env main_train.py \
+--model deit_tiny_patch16_224 --batch-size 256 --data-path /root/data/imagenet/ --output_dir /root/checkpoints_trang/rpc_tiny/ --hyperparam_1 0.9 --hyperparam_2 1.0 --method moving_average &
+CUDA_VISIBLE_DEVICES='4,5,6,7' python -m torch.distributed.launch --master_port 10020 --nproc_per_node=4 --use_env main_train.py \
+--model deit_tiny_patch16_224 --batch-size 256 --data-path /root/data/imagenet/ --output_dir /root/checkpoints_trang/rpc_tiny/ --u 0.3 --s 0.1 --method momentum &
+CUDA_VISIBLE_DEVICES='4,5,6,7' python -m torch.distributed.launch --master_port 10020 --nproc_per_node=4 --use_env main_train.py \
+--model deit_tiny_patch16_224 --batch-size 256 --data-path /root/data/imagenet/ --output_dir /root/checkpoints_trang/rpc_tiny/ --u 0.1 --s 0.1 --method momentum &
